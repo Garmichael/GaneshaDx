124de10c93c15513af32e0a7210335630dcfb0a0
Garmichael <Garmichael@gmail.com>
2022-06-09 07:13:50 -0700

    Nail down the Configuration of the gltf MaterialBuilder
    Took 2 hours 4 minutes

--- Common/GltfExport.cs
+++ Common/GltfExport.cs
@@ -18,21 +18,21 @@ using MTex2D = Microsoft.Xna.Framework.Graphics.Texture2D;
 using GaneshaDx.Resources.ContentDataTypes.Palettes;
 using GaneshaDx.Resources.ContentDataTypes.TextureAnimations;
 using SharpGLTF.Scenes;
+using AlphaMode = SharpGLTF.Materials.AlphaMode;
 
-namespace GaneshaDx.Common
-{
-  public static class GltfExport {
-
-		private const float _reduceScaleFactor = 50;
+namespace GaneshaDx.Common {
+	public static class GltfExport {
+		private const float ReduceScaleFactor = 50;
 
 		private static List<Color> _greyPalette = new List<Color>();
-		public static List<Color> GreyPalette {
+
+		private static List<Color> GreyPalette {
 			get {
 				if (_greyPalette.Count > 0) {
 					return _greyPalette;
 				}
 
-				_greyPalette = new List<Color>() {
+				_greyPalette = new List<Color> {
 					Utilities.GetColorFromHex("000000"),
 					Utilities.GetColorFromHex("111111"),
 					Utilities.GetColorFromHex("222222"),
@@ -57,12 +57,13 @@ namespace GaneshaDx.Common
 
 		public static void Export(string filePath) {
 			MeshBuilder<VertexPosition, VertexTexture1> texturedMesh = new MeshBuilder<VertexPosition, VertexTexture1>("TexturedMesh");
-			List<PrimitiveBuilder<MaterialBuilder, VertexPosition, VertexTexture1, VertexEmpty>> texturedPrimitives = new List<PrimitiveBuilder<MaterialBuilder, VertexPosition, VertexTexture1, VertexEmpty>>();
+			List<PrimitiveBuilder<MaterialBuilder, VertexPosition, VertexTexture1, VertexEmpty>> texturedPrimitives =
+				new List<PrimitiveBuilder<MaterialBuilder, VertexPosition, VertexTexture1, VertexEmpty>>();
 			CreateTextures(texturedPrimitives, texturedMesh);
 
 			MeshBuilder<VertexPosition> blackMesh = new MeshBuilder<VertexPosition>("BlackMesh");
 			MaterialBuilder blackMaterial = new MaterialBuilder();
-			blackMaterial.WithBaseColor(new Vector4(0,0,0,1));
+			blackMaterial.WithBaseColor(new Vector4(0, 0, 0, 1));
 			PrimitiveBuilder<MaterialBuilder, VertexPosition, VertexEmpty, VertexEmpty> blackPrimitive = blackMesh.UsePrimitive(blackMaterial);
 
 			foreach (Polygon polygon in CurrentMapState.StateData.PolygonCollectionBucket) {
@@ -80,9 +81,9 @@ namespace GaneshaDx.Common
 						);
 					} else {
 						blackPrimitive.AddQuadrangle(
-							ConvertAndScaleVector3ToVertexPosition(vertices[2].Position), 
-							ConvertAndScaleVector3ToVertexPosition(vertices[3].Position), 
-							ConvertAndScaleVector3ToVertexPosition(vertices[1].Position), 
+							ConvertAndScaleVector3ToVertexPosition(vertices[2].Position),
+							ConvertAndScaleVector3ToVertexPosition(vertices[3].Position),
+							ConvertAndScaleVector3ToVertexPosition(vertices[1].Position),
 							ConvertAndScaleVector3ToVertexPosition(vertices[0].Position)
 						);
 					}
@@ -97,8 +98,8 @@ namespace GaneshaDx.Common
 						);
 					} else {
 						blackPrimitive.AddTriangle(
-							ConvertAndScaleVector3ToVertexPosition(verts[2].Position), 
-							ConvertAndScaleVector3ToVertexPosition(verts[1].Position), 
+							ConvertAndScaleVector3ToVertexPosition(verts[2].Position),
+							ConvertAndScaleVector3ToVertexPosition(verts[1].Position),
 							ConvertAndScaleVector3ToVertexPosition(verts[0].Position)
 						);
 					}
@@ -122,51 +123,46 @@ namespace GaneshaDx.Common
 		}
 
 		private static Vector3 ConvertAndScaleVector3(Microsoft.Xna.Framework.Vector3 vector3) {
-			return new Vector3(vector3.X / _reduceScaleFactor, vector3.Y / _reduceScaleFactor, vector3.Z / _reduceScaleFactor);
+			return new Vector3(vector3.X / ReduceScaleFactor, vector3.Y / ReduceScaleFactor, vector3.Z / ReduceScaleFactor);
 		}
 
-		private static VertexPosition ConvertAndScaleVector3ToVertexPosition (Microsoft.Xna.Framework.Vector3 vector3) {
-			return new VertexPosition(vector3.X / _reduceScaleFactor, vector3.Y / _reduceScaleFactor, vector3.Z / _reduceScaleFactor);
+		private static VertexPosition ConvertAndScaleVector3ToVertexPosition(Microsoft.Xna.Framework.Vector3 vector3) {
+			return new VertexPosition(vector3.X / ReduceScaleFactor, vector3.Y / ReduceScaleFactor, vector3.Z / ReduceScaleFactor);
 		}
 
-		private static Vector2 GetAdjustedUvCoordinates (Microsoft.Xna.Framework.Vector2 uv, int texturePage) {
+		private static Vector2 GetAdjustedUvCoordinates(Microsoft.Xna.Framework.Vector2 uv, int texturePage) {
 			double adjustedX = uv.X / 256f;
 			double adjustedY = (uv.Y + 256 * texturePage) / 1024f;
 			return new Vector2((float) adjustedX, (float) adjustedY);
 		}
 
-		private static void CreateTextures (List<PrimitiveBuilder<MaterialBuilder, VertexPosition, VertexTexture1, VertexEmpty>> texturedPrimitives, MeshBuilder<VertexPosition, VertexTexture1> texturedMesh) {
+		private static void CreateTextures(List<PrimitiveBuilder<MaterialBuilder, VertexPosition, VertexTexture1, VertexEmpty>> texturedPrimitives,
+			MeshBuilder<VertexPosition, VertexTexture1> texturedMesh) {
 			MTex2D stateTexture = CurrentMapState.StateData.Texture;
 
-			foreach(Palette palette in CurrentMapState.StateData.Palettes) {
+			foreach (Palette palette in CurrentMapState.StateData.Palettes) {
 				Color[] textureColors = new Color[stateTexture.Width * stateTexture.Height];
-				stateTexture.GetData<Color>(textureColors);
+				stateTexture.GetData(textureColors);
 
 				ApplyPalette(textureColors, palette);
 
 				byte[] textureBytes = GeneratePngBytesFromColors(textureColors);
 
-				MaterialBuilder material = new MaterialBuilder();
-				// @todo: Tweak the material so that it exports as close to possible as game-rendered version.
-				//material.WithMetallicRoughness(0, 0);
-				//material.WithUnlitShader(); // this looks close but material doesn't get lighting at all.
-
 				MemoryImage memoryImage = new MemoryImage(textureBytes);
-
-				// @todo: Why do specular options always crash on export?
-				//material.WithSpecularGlossinessShader()
-				//	.WithDiffuse(memoryImage, new Vector4(0))
-				//	.WithSpecularGlossiness(memoryImage, new Vector3(0), 0f);
-
+				MaterialBuilder material = new MaterialBuilder().WithAlpha(AlphaMode.MASK);
+				material.WithMetallicRoughnessShader();
+				material.WithMetallicRoughness(0, 1);
 				material.WithChannelImage(KnownChannel.BaseColor, memoryImage);
-				material.GetChannel(KnownChannel.BaseColor)
-                .Texture
-                .WithSampler(
-									TextureWrapMode.CLAMP_TO_EDGE, 
-									TextureWrapMode.MIRRORED_REPEAT, 
-									TextureMipMapFilter.NEAREST_MIPMAP_NEAREST, 
-									TextureInterpolationFilter.NEAREST
-								);
+				material.UseChannel(KnownChannel.BaseColor)
+					.Texture
+					.WithSampler(
+						TextureWrapMode.CLAMP_TO_EDGE,
+						TextureWrapMode.MIRRORED_REPEAT,
+						TextureMipMapFilter.NEAREST_MIPMAP_NEAREST,
+						TextureInterpolationFilter.NEAREST
+					);
+				material.WithSpecularFactor(memoryImage, 0);
+				material.WithSpecularColor(memoryImage, Vector3.Zero);
 
 				texturedPrimitives.Add(texturedMesh.UsePrimitive(material));
 			}
@@ -178,7 +174,7 @@ namespace GaneshaDx.Common
 
 			if (usesTextureAnimations) {
 				int paletteIndex = CurrentMapState.StateData.Palettes.IndexOf(palette);
-				
+
 				foreach (AnimatedTextureInstructions instruction in CurrentMapState.StateData.TextureAnimations) {
 					if (instruction.TextureAnimationType == TextureAnimationType.PaletteAnimation) {
 						PaletteAnimation paletteAnimation = (PaletteAnimation) instruction.Instructions;
@@ -203,7 +199,7 @@ namespace GaneshaDx.Common
 		private static byte[] GeneratePngBytesFromColors(Color[] textureColors) {
 			MTex2D stateTexture = CurrentMapState.StateData.Texture;
 			MTex2D texture = new MTex2D(Stage.Ganesha.GraphicsDevice, stateTexture.Width, stateTexture.Height);
-			texture.SetData<Color>(textureColors);
+			texture.SetData(textureColors);
 			MemoryStream stream = new MemoryStream();
 			texture.SaveAsPng(stream, stateTexture.Width, stateTexture.Height);
 			return stream.ToArray();
