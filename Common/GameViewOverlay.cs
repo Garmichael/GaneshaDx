e09371d195868c13364ff34939df02de19a13153
Garmichael <Garmichael@gmail.com>
2024-05-05 03:37:53 -0700

    Factored the Ortho Distortion into the Game View Overlay

--- Common/GameViewOverlay.cs
+++ Common/GameViewOverlay.cs
@@ -6,9 +6,13 @@ namespace GaneshaDx.Common {
 	public static class GameViewOverlay {
 		public static void Render() {
 			if (MapData.MapIsLoaded && Configuration.Properties.ShowGameViewOverlay) {
+				float orthoDistortion = Configuration.Properties.RenderFftOrtho
+					? 1
+					: 5f / 4f;
+				
 				Vector2 center = new Vector2(Stage.ModelingViewport.Width / 2f, Stage.ModelingViewport.Height / 2f);
 				float width = 256 / (float) StageCamera.ZoomLevel;
-				float height = 190 / (float) StageCamera.ZoomLevel;
+				float height = 190 / (float) StageCamera.ZoomLevel * orthoDistortion;
 
 				Rectangle screenRect = new Rectangle {
 					X = (int) (center.X - width / 2),
