d185411729d5ae189df97771d80b4e0bc9f921ff
Garmichael <Garmichael@gmail.com>
2024-05-05 12:28:25 -0700

    Added a border to Game View Overlay

--- Common/GameViewOverlay.cs
+++ Common/GameViewOverlay.cs
@@ -6,13 +6,17 @@ namespace GaneshaDx.Common {
 	public static class GameViewOverlay {
 		public static void Render() {
 			if (MapData.MapIsLoaded && Configuration.Properties.ShowGameViewOverlay) {
+				Vector2 screenDimensions = new Vector2(256, 190);
+				int borderWidth = 1;
+				Color borderColor = Color.White;
+
 				float orthoDistortion = Configuration.Properties.RenderFftOrtho
 					? 1
 					: 5f / 4f;
-				
+
 				Vector2 center = new Vector2(Stage.ModelingViewport.Width / 2f, Stage.ModelingViewport.Height / 2f);
-				float width = 256 / (float) StageCamera.ZoomLevel;
-				float height = 190 / (float) StageCamera.ZoomLevel * orthoDistortion;
+				float width = screenDimensions.X / (float) StageCamera.ZoomLevel;
+				float height = screenDimensions.Y / (float) StageCamera.ZoomLevel * orthoDistortion;
 
 				Rectangle screenRect = new Rectangle {
 					X = (int) (center.X - width / 2),
@@ -22,7 +26,7 @@ namespace GaneshaDx.Common {
 				};
 
 				Stage.SpriteBatch.Begin();
-				
+
 				Stage.SpriteBatch.Draw(
 					UniversalTextures.SemiTransparentBlackTexture,
 					new Rectangle(0, 0, Stage.ModelingViewport.Width, screenRect.Top)
@@ -44,6 +48,30 @@ namespace GaneshaDx.Common {
 					Color.White
 				);
 
+				Stage.SpriteBatch.Draw(
+					UniversalTextures.WhiteTexture,
+					new Rectangle(screenRect.Left - borderWidth, screenRect.Top - borderWidth, (int) width + borderWidth * 2, borderWidth),
+					borderColor
+				);
+
+				Stage.SpriteBatch.Draw(
+					UniversalTextures.WhiteTexture,
+					new Rectangle(screenRect.Left - borderWidth, screenRect.Top - borderWidth, borderWidth, (int) height + borderWidth * 2),
+					borderColor
+				);
+				
+				Stage.SpriteBatch.Draw(
+					UniversalTextures.WhiteTexture,
+					new Rectangle(screenRect.Right, screenRect.Top - borderWidth, borderWidth, (int) height + borderWidth * 2),
+					borderColor
+				);
+
+				Stage.SpriteBatch.Draw(
+					UniversalTextures.WhiteTexture,
+					new Rectangle(screenRect.Left - borderWidth, screenRect.Bottom, (int) width + borderWidth * 2, borderWidth),
+					borderColor
+				);
+				
 				Stage.SpriteBatch.End();
 			}
 		}
