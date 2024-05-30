c48a0900cdc4d16e4e1848c9742b2fbde992e76f
Garret Bright <Garmichael@gmail.com>
2024-05-30 11:26:05 -0700

    Move Preferences to AppData folder

--- Common/Configuration.cs
+++ Common/Configuration.cs
@@ -6,10 +6,16 @@ namespace GaneshaDx.Common;
 
 public static class Configuration {
 	private const string ConfigurationFile = "Preferences.json";
+	private static string _configurationPath;
 	public static ConfigurationProperties Properties;
 
 	public static void LoadConfiguration() {
-		if (!File.Exists(ConfigurationFile)) {
+		_configurationPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.ApplicationData) + "\\GaneshaDx" + "\\";
+		if (!File.Exists(_configurationPath + ConfigurationFile)) {
+			if (!Directory.Exists(_configurationPath)) {
+				Directory.CreateDirectory(_configurationPath);
+			}
+			
 			Properties = new ConfigurationProperties();
 			SaveConfiguration();
 		} else {
@@ -23,7 +29,7 @@ public static class Configuration {
 	}
 
 	public static void SaveConfiguration() {
-		FileStream stream = new(ConfigurationFile, FileMode.Create);
+		FileStream stream = new(_configurationPath + ConfigurationFile, FileMode.Create);
 		using (StreamWriter writer = new(stream, Encoding.Default)) {
 			writer.WriteLine(JsonConvert.SerializeObject(Properties, Formatting.Indented));
 		}
